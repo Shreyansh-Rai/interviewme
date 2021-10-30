9edfbf7a8c12b1407605e4e64d1f4cc92626cd0d
vatsal007 <75558599+vatsal-dhama@users.noreply.github.com>
2021-10-31 00:13:26 +0530

    Update Meetings.js

--- interview/src/pages/Meetings.js
+++ interview/src/pages/Meetings.js
@@ -66,12 +66,7 @@ function url_redirect(url){
   return false;
 };
     // console.log(nameme)
-  const sty = {
-    boxsizing: "border-box",
-    padding: "2vh",
-    margin: "20px 20px 20px 20px",
-    borderradius: "10px",
-  };
+  
   const ty = {
       display : "flex",
       justifyContent : "Space-between",
@@ -82,116 +77,44 @@ function url_redirect(url){
 //   console.log(nameme)
   return (
     <>
-    {/* <Header/> */}
-      {/* <div style={sty} className="meeting_card_flex">
-        
-          {
-            meeting_list.map((i) =>{
-            <div className="card">
-            <div className="card-header">Meeting 1: </div>
-              <div className="card-body">
-                <h5 className="card-title">{nameme}</h5>
-                <div style= {ty}>
-                    <Link to = "/Meetings/Get1"><button className="btn btn-primary">Get data</button></Link>
-                    <button className="btn btn-primary" onClick = {()=>url_redirect(url)}>start call</button>
-                    </div>
-              </div>
-            </div>
-          })
-              
-        }
-      </div> */}
+   
       <Header/>
-      <div className="meeting_card_flex" div style={sty}>
+      <div className="meeting_card_flex" >
       {
         meeting_list.map((i) => (
             
-            <div className="card" >
-              <div key={i[2]} className="card-header">{i[0]}</div>
-              <div className="card-body">
-                <h5 key={i[2]} className="card-title">{i[1]}</h5>
+            <div className="card_" >
+              <div key={i[2]} className="card-header_">{i[0]}</div>
+              
                 
-                <div style= {ty}>
+                <div className="flex_meeting_">
+                    <p className="flex_meeting_left">Interviewee:</p>
+                    <p className="flex_meeting_right">{i[1]}</p>
+                </div>
+                <hr/>
+                <div className="flex_meeting_2">
                     <Link to = {() =>{
                       const getstr = "/Meetings/Get/" + i[2].toString();
                       return getstr;
-                    }}><button className="btn btn-primary">Get data</button></Link>
-                    <button className="btn btn-primary" onClick = {()=>{
+                    }}  style={{textDecoration:"none"}} ><p className="buttons_">Get data</p></Link>
+                    
+                    <p className="buttons_" onClick = {()=>{
                       const urln = url + i[2].toString()
-                      url_redirect(urln)}}>start call</button>
+                      url_redirect(urln)}}>Start call</p>
+
                     <Link to = {() =>{
                       const getstr = "/Meetings/Accept/" + i[2].toString();
                       return getstr;
-                    }}><button className="btn btn-primary">Accept Applicant</button></Link>
-                    </div>
-              </div>
+                    }} style={{textDecoration:"none"}} ><p className="buttons_">Accept Applicant</p></Link>
+                </div>
+              
               
             </div>
         ))
       }
       </div>
     
-      {/* <div style={sty}>
-        <div className="card">
-          <div className="card-header">Meeting 2 :</div>
-          <div className="card-body">
-            <h5 className="card-title">Interviewee 2</h5>
-                <div style= {ty}>
-                <Link to = "/Meetings/Get2"><button className="btn btn-primary">Get data</button></Link>
-                <Link to = "/Meetings/callme2"><button className="btn btn-primary">start call</button></Link>
-                </div>
-          </div>
-        </div>
-
-      </div>
-      <div style={sty}>
-        <div className="card">
-          <div className="card-header">Meeting 3 :</div>
-          <div className="card-body">
-            <h5 className="card-title">Interviewee 3</h5>
-                <div style= {ty}>
-                <Link to = "/Meetings/Get3"><button className="btn btn-primary">Get data</button></Link>
-                <Link to = "/Meetings/callme3"><button className="btn btn-primary">start call</button></Link>
-                </div>
-          </div>
-        </div>
-
-      </div>
-      <div style={sty}>
-        <div className="card">
-          <div className="card-header">Meeting 4 :</div>
-          <div className="card-body">
-            <h5 className="card-title">Interviewee 4</h5>
-                <div style= {ty}>
-                <Link to = "/Meetings/Get4"><button className="btn btn-primary">Get data</button></Link>
-                <Link to = "/Meetings/callme4"><button className="btn btn-primary">start call</button></Link>
-                </div>
-          </div>
-        </div>
-
-      </div> */}
+      
     </>
   );
 }
-// const [hisec, sethisec] = useState("");
-//   const [ungraddeg, setungraddeg] = useState("");
-//   const [ungradclg, setungradclg] = useState("");
-//   const [postgraddeg, setpostgraddeg] = useState("");
-//   const [postgradclg, setpostgradclg] = useState("");
-//   const [pexp1, setpexp1] = useState("");
-//   const [pexp2, setpexp2] = useState("");
-//   const [pexp3, setpexp3] = useState("");
-// if(data.hasOwnProperty("undergrad_degree"))
-      // {setungraddeg(data.undergrad_degree);}
-      // if(data.hasOwnProperty("undergrad_college"))
-      // {setungradclg(data.undergrad_college);}
-      // if(data.hasOwnProperty("postgrad_degree"))
-      // {setpostgraddeg(data.postgrad_degree);}
-      // if(data.hasOwnProperty("postgrad_college"))
-      // {setpostgradclg(data.postgrad_colleg);}
-      // if(data.hasOwnProperty("past_experience1"))
-      // {setpexp1(data.past_experience1);}
-      // if(data.hasOwnProperty("past_experience2"))
-      // {setpexp2(data.past_experience2);}
-      // if(data.hasOwnProperty("past_experience3"))
-      // {setpexp3(data.past_experience3);}
